27daf754cc2d0ce218fee29715d6cdc7a5a2c11a
Alejandro Gil <gelu170@gmail.com>
2020-07-14 15:14:07 -0500

    Creación de pruebas unitarias

---
+++ FrameworkTest/CalculadoraTest.cs
@@ -0,0 +1,89 @@
+﻿using Framework.Utilidades;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrameworkTest
+{
+    public class CalculadoraTest
+    {
+        private Calculadora objCalculadora;
+
+        [SetUp]
+        public void Setup()
+        {
+            objCalculadora = new Calculadora();
+        }
+
+        [Test]
+        public void SumarTest()
+        {
+            //Setup - Configuracion
+            int numero1 = 5;
+            int numero2 = 10;
+            int resultadoEsperado = 15;
+
+            //Calculadora objCalculadora = new Calculadora();
+
+            //Arrange - Ejecución
+            var resultado = objCalculadora.Sumar(numero1, numero2);
+
+            //Assert - Prueba
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [Test]
+        public void RestarTest()
+        {
+            //Setup - Configuracion
+            int numero1 = 15;
+            int numero2 = 10;
+            int resultadoEsperado = 5;
+
+            //Calculadora objCalculadora = new Calculadora();
+
+            //Arrange - Ejecución
+            var resultado = objCalculadora.Restar(numero1, numero2);
+
+            //Assert - Prueba
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [Test]
+        public void DividirTest()
+        {
+            //Setup - Configuracion
+            int numero1 = 9;
+            int numero2 = 3;
+            int resultadoEsperado = 3;
+
+            //Calculadora objCalculadora = new Calculadora();
+
+            //Arrange - Ejecución
+            var resultado = objCalculadora.Dividir(numero1, numero2);
+
+            //Assert - Prueba
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [Test]
+        public void DividirZeroTest()
+        {
+            //Setup - Configuracion
+            int numero1 = 9;
+            int numero2 = 0;
+            int resultadoEsperado = 0;
+
+            //Calculadora objCalculadora = new Calculadora();
+
+            //Arrange - Ejecución
+            var resultado = objCalculadora.Dividir(numero1, numero2);
+
+            //Assert - Prueba
+            //Assert.Throws<DivideByZeroException>(() => objCalculadora.Dividir(numero1, numero2));
+            Assert.AreEqual(resultado, resultadoEsperado);
+        }
+
+    }
+}
